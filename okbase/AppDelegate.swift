194290d397bafd750ded68bd577cde3f35b87cf7
JediZ90 <imbh.zhang@outlook.com>
2020-06-14 21:29:44 +0800

    init

---
+++ okbase/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  okbase
+//
+//  Created by 张宝浩 on 2020/6/14.
+//  Copyright © 2020 okbase. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ okbase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  okbase
+//
+//  Created by 张宝浩 on 2020/6/14.
+//  Copyright © 2020 okbase. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
